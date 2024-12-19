08120df48d8a2a8d17b69a9320b65fe5576458db
Sarah Pfeifer <sarahpfeifer@Sarahs-MacBook-Pro-2.local>
2024-12-19 09:55:36 -0700

    Data cleaned, ready for models.

--- autism.py
+++ autism.py
@@ -1,7 +1,16 @@
-
-
 from ucimlrepo import fetch_ucirepo 
-  
+import scipy as sp
+import scipy.stats as stats
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import copy
+# Set color map to have light blue background
+sns.set()
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+ 
 # fetch dataset 
 autism_screening_adult = fetch_ucirepo(id=426) 
   
@@ -12,5 +21,18 @@ y = autism_screening_adult.data.targets
 # metadata 
 print(autism_screening_adult.metadata) 
   
-# variable information 
+# variable information
 print(autism_screening_adult.variables) 
+X.jaundice.replace(('yes', 'no'), (1, 0), inplace=True)
+X.family_pdd.replace(('yes', 'no'), (1, 0), inplace=True)
+X.gender.replace(('m', 'f'), (1,0), inplace=True)
+X.age.replace(383, 38, inplace=True)
+X.ethnicity.replace('others', 'Others', inplace=True)
+X['ethnicity'].fillna('Others', inplace=True)
+X.drop(['age_desc', 'relation', 'used_app_before'], axis=1, inplace=True)
+mean_value = int(X['age'].mean())
+X['age'].fillna(value=mean_value, inplace=True) 
+for c in X.columns[:]:
+    print(c, X[c].unique())
+X['age'].fillna
+#correlations = X.corr()
